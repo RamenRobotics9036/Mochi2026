129400aa965147322e1f96f3b35ad4f710feb5e8
Ramen bot Sam1 <ramen9036bot1@gmail.com>
2026-02-07 13:35:40 -0800

    Command for testing

---
+++ src/main/java/frc/robot/commands/ShooterTestCommand.java
@@ -0,0 +1,41 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class ShooterTestCommand extends Command {
+    private final ShooterSubsystem m_shooter;
+
+    private CommandXboxController m_controller;
+
+    public ShooterTestCommand(ShooterSubsystem shooter, CommandXboxController controller) {
+        m_shooter = shooter;
+        m_controller = controller;
+        addRequirements(m_shooter);
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+    }
+
+    // Called every time the scheduler runs while the command is scheduled.
+    @Override
+    public void execute() {
+        m_shooter.SetSpeed(m_controller.getLeftY());
+    }
+
+    // Called once the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        m_shooter.stop();
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+    
+}
