fb4d5f853b422986c018e57352ac653bc689df6d
Ramen bot Sam1 <ramen9036bot1@gmail.com>
2026-02-07 12:32:33 -0800

    Convert from TalonFX to SparkFlex

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,24 +1,43 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
+import edu.wpi.first.wpilibj.Servo;
 import frc.robot.Constants.ShooterConstants;
 
 public class ShooterSubsystem {
-    private TalonFX m_lMotor;
-    private TalonFX m_rMotor;
+    private SparkFlex m_lMotor;
+    private SparkFlex m_rMotor;
+
+    private SparkFlexConfig m_lConfig;
+    private SparkFlexConfig m_rConfig;
+
     private Servo m_hood;
 
     public ShooterSubsystem(){
-        m_lMotor = new TalonFX(ShooterConstants.kLMotorID);
-        m_rMotor = new TalonFX(ShooterConstants.kRMotorID);
+        m_lMotor = new SparkFlex(ShooterConstants.kLMotorID, MotorType.kBrushless);
+        m_rMotor = new SparkFlex(ShooterConstants.kRMotorID, MotorType.kBrushless);
+
+        m_lConfig = new SparkFlexConfig();
+        m_rConfig = new SparkFlexConfig();
+
+        m_lConfig.idleMode(IdleMode.kBrake)
+            .smartCurrentLimit(ShooterConstants.kCurrentLimit);
+
+        m_rConfig.idleMode(IdleMode.kBrake)
+            .smartCurrentLimit(ShooterConstants.kCurrentLimit)
+            .inverted(true)
+            .follow(m_lMotor);
+
         m_hood = new Servo(ShooterConstants.kChannel);
     }
 
     public void SetSpeed(double speed){
         m_lMotor.set(speed);
-        //Facing the opposite direction.
-        m_rMotor.set(-speed);
     }
     
     //Gets the speed of the uninverted left motor
