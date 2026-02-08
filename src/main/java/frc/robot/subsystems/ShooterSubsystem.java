c684f4571473a5ec50c267742b89b43a05c9211c
Asher Bean <asherbean380@gmail.com>
2026-02-07 17:53:01 -0800

    Fixed configuration

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
-import com.ctre.phoenix6.hardware.TalonFX;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkFlexConfig;
@@ -23,31 +24,26 @@ public class ShooterSubsystem extends SubsystemBase {
         m_lMotor = new SparkFlex(ShooterConstants.kLMotorID, MotorType.kBrushless);
         m_rMotor = new SparkFlex(ShooterConstants.kRMotorID, MotorType.kBrushless);
 
-        // $TODO - Asher: The m_lConfig and m_rConfig are created here, but never set on the motor.
-        // Compare this to what you do in IntakeSubsystem:
-        //   m_intakeMotor.configure(m_config,
-        //       SparkBase.ResetMode.kResetSafeParameters,
-        //       SparkBase.PersistMode.kPersistParameters);
         m_lConfig = new SparkFlexConfig();
         m_rConfig = new SparkFlexConfig();
 
         m_lConfig.idleMode(IdleMode.kBrake)
             .smartCurrentLimit(ShooterConstants.kCurrentLimit);
-
         m_rConfig.idleMode(IdleMode.kBrake)
             .smartCurrentLimit(ShooterConstants.kCurrentLimit)
             .inverted(true)
             .follow(m_lMotor);
 
+        m_lMotor.configure(m_lConfig, ResetMode.kResetSafeParameters,
+                PersistMode.kPersistParameters);
+        m_lMotor.configure(m_lConfig, ResetMode.kResetSafeParameters,
+                PersistMode.kPersistParameters);
+
         m_hood = new Servo(ShooterConstants.kChannel);
     }
 
     public void setSpeed(double speed){
-        // $TODO - Asher: By setting motor speed, the shooting will not be very accurate
-        // (just setting speed is called 'open loop'). You want to use a closed-circuit loop
-        // (in other words 'PID') so that the encoders and PID logic are making sure the motors
-        // are moving at the exact right speed.  It's OK to just put this as a todo for now to
-        // remind us in-case our shooting distance isn't as accurate as we expect later.
+        // $TODO: Use PID to ensure the motors are spinning at exactly the right speed.
         m_lMotor.set(speed);
     }
 
