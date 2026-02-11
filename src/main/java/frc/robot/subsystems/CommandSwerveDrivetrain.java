92527e4568ed619bd7da159aed62f5e96ddce927
BowedMars374878 <bowedmars@gmail.com>
2026-02-10 20:54:28 -0800

    Finally gets AprilTag alignment command working! -Ronen

--- src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CommandSwerveDrivetrain.java
@@ -20,6 +20,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.Notifier;
@@ -263,8 +264,8 @@ public class CommandSwerveDrivetrain extends TunerSwerveDrivetrain implements Su
      *
      * @return Command to run
      */
-    public Command AlignToTag(CommandXboxController joystick) {
-        return new AlignToTagCommand(this, joystick);
+    public Command AlignToTag(CommandXboxController joystick, double MaxAngularVelocity) {
+        return new AlignToTagCommand(this, joystick, MaxAngularVelocity);
     }
 
     /**
