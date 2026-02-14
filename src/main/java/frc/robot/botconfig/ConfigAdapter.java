01d0cac6b741d8eb53aeab1baef34e4d24b7d590
Ido <idoben2@hotmail.com>
2026-02-14 12:19:59 -0800

    Adapter for config

---
+++ src/main/java/frc/robot/botconfig/ConfigAdapter.java
@@ -0,0 +1,72 @@
+package frc.robot.botconfig;
+
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.swerve.SwerveModuleConstants;
+import com.ctre.phoenix6.swerve.SwerveDrivetrainConstants;
+import edu.wpi.first.units.measure.LinearVelocity;
+import frc.robot.generated.TunerConstants;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+
+/**
+ * Adapter that bridges BotConfigInterface to the current TunerConstants.
+ * For now this delegates directly to TunerConstants; when a second robot
+ * is added, this class can be swapped or extended to select between configs.
+ */
+public class ConfigAdapter implements BotConfigInterface {
+
+    @Override
+    public LinearVelocity getSpeedAt12Volts() {
+        return TunerConstants.kSpeedAt12Volts;
+    }
+
+    @Override
+    public LinearVelocity getSpeedInTeleop() {
+        return TunerConstants.kSpeedInTeleop;
+    }
+
+    @Override
+    public double getAlignmentErrorMargin() {
+        return TunerConstants.kAlignmentErrorMargin;
+    }
+
+    @Override
+    public double getSwerveMoveInterruptionSensitivity() {
+        return TunerConstants.kSwerveMoveInterruptionSensitivity;
+    }
+
+    @Override
+    public double getSwerveTurnInterruptionSensitivity() {
+        return TunerConstants.kSwerveTurnInterruptionSensitivity;
+    }
+
+    @Override
+    public SwerveDrivetrainConstants getDrivetrainConstants() {
+        return TunerConstants.DrivetrainConstants;
+    }
+
+    @Override
+    public SwerveModuleConstants<TalonFXConfiguration, TalonFXConfiguration, CANcoderConfiguration> getFrontLeft() {
+        return TunerConstants.FrontLeft;
+    }
+
+    @Override
+    public SwerveModuleConstants<TalonFXConfiguration, TalonFXConfiguration, CANcoderConfiguration> getFrontRight() {
+        return TunerConstants.FrontRight;
+    }
+
+    @Override
+    public SwerveModuleConstants<TalonFXConfiguration, TalonFXConfiguration, CANcoderConfiguration> getBackLeft() {
+        return TunerConstants.BackLeft;
+    }
+
+    @Override
+    public SwerveModuleConstants<TalonFXConfiguration, TalonFXConfiguration, CANcoderConfiguration> getBackRight() {
+        return TunerConstants.BackRight;
+    }
+
+    @Override
+    public CommandSwerveDrivetrain createDrivetrain() {
+        return TunerConstants.createDrivetrain();
+    }
+}
