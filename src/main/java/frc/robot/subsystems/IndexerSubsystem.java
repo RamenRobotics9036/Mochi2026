b63e00836e482e2764543420d618aad33f30cb51
Ramen9036Bot1 <ramen9036bot1@gmail.com>
2026-02-16 15:13:33 -0800

    changes to adding the shooter

---
+++ src/main/java/frc/robot/subsystems/IndexerSubsystem.java
@@ -0,0 +1,39 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkFlexConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IndexerConstants;
+import frc.robot.Constants.ShooterConstants;
+
+public class IndexerSubsystem extends SubsystemBase{
+    private SparkFlex m_motor;
+    private SparkFlexConfig m_config;
+
+    public IndexerSubsystem(){
+        m_motor = new SparkFlex(IndexerConstants.kMotorID, MotorType.kBrushless);
+
+        m_config.idleMode(IdleMode.kBrake)
+            .smartCurrentLimit(ShooterConstants.kCurrentLimit);
+
+        m_motor.configure(m_config, ResetMode.kResetSafeParameters, 
+            PersistMode.kPersistParameters);
+    }
+
+    public void setSpeed(double speed){
+        m_motor.set(speed);
+    }
+
+    public double getSpeed(){
+        return m_motor.get();
+    }
+
+    public void stop(){
+        m_motor.stopMotor();
+    }
+}
