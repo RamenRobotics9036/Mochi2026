a859adcd1d9091bfd697e2751ce0f8c369894e26
Ramen9036Bot1 <ramen9036bot1@gmail.com>
2026-02-16 16:46:14 -0800

    added the file for the default command

---
+++ src/main/java/frc/robot/commands/ShooterDefaultCommand.java
@@ -0,0 +1,44 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants.IndexerConstants;
+import frc.robot.Constants.ShooterConstants;
+import frc.robot.subsystems.IndexerSubsystem;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class ShooterDefaultCommand extends Command{
+    private ShooterSubsystem m_shooter;
+    private IndexerSubsystem m_indexer;
+    private CommandXboxController m_controller;
+
+    public ShooterDefaultCommand(ShooterSubsystem shooter, IndexerSubsystem indexer, CommandXboxController controller){
+        m_shooter = shooter;
+        m_indexer = indexer;
+        m_controller = controller;
+
+        addRequirements(m_shooter, m_indexer);
+    }
+
+    @Override
+    public void initialize() {}
+
+    @Override
+    public void execute() {
+        if(m_controller.getRightTriggerAxis() > 0){
+            m_shooter.setSpeed(ShooterConstants.kShootSpeed);
+            m_indexer.setSpeed(IndexerConstants.kIndexSpeed);
+        }
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        m_shooter.stop();
+        m_indexer.stop();
+    }
+}
