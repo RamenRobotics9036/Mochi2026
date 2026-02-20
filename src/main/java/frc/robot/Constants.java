1e2780ad32e3b1776135afa16730c0f0746a87ee
Ido <idoben2@hotmail.com>
2026-02-19 17:05:28 -0800

    Cleanup

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -31,7 +31,8 @@ public final class Constants {
         /** Default speed for raising and lowering the intake arm. */
         public static final double kArmSpeed = 0.05;
         /** Current threshold in Amps used to detect if a game piece is fully secured. */
-        public static final int kStallLimit = 40; // Amps
+        public static final int kIntakeRollerStallLimit = 40;
+        public static final int kArmStallLimit = 40;
 
         public static final int kLeftArmMotorID = 20;
         public static final int kRightArmMotorID = 21;
