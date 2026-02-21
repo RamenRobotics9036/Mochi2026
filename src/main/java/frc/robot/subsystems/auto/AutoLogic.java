4501289e386bff6f23e8a18353370e2eb17fbd99
Ido <idoben2@hotmail.com>
2026-02-21 14:05:38 -0800

    Cleanup

--- src/main/java/frc/robot/subsystems/auto/AutoLogic.java
+++ src/main/java/frc/robot/subsystems/auto/AutoLogic.java
@@ -90,7 +90,7 @@ public final class  AutoLogic {
         autoPicker.addOption("Center Auto", "Center Auto");
         autoPicker.addOption("Scale test", "Scale test");
         autoPicker.addOption("Diagonal path", "Diagonal path");
-        autoPicker.setDefaultOption("Cross", "Cross");
+        autoPicker.addOption("Cross", "Cross");
         autoPicker.setDefaultOption("testing shoot", "testing shoot");
     }
 
