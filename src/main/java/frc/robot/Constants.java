8d0d1de464757a4d04358416d245318048a41fd5
Ido <idoben2@hotmail.com>
2026-02-20 17:16:59 -0800

    consistent accuracy for reliability

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -114,7 +114,7 @@ public final class Constants {
         public static final int kHoodPwmChannel = 0; //TODO: filler value
 
         /** Shooting speed */
-        public static final double kShootSpeed = 0.40; //TODO: filler value
+        public static final double kShootSpeed = 0.60; //TODO: filler value
 
         public static final double kShooterGearRatio = 20.0; //TODO: filler value
     }
@@ -147,7 +147,7 @@ public final class Constants {
         /** Max power (0.5 = 50% power). */
         public static final double kMaxOutputPercent = 0.40;
         /** Amps limit (higher for single motor lifting full weight). */
-        public static final int kCurrentLimit = 60;
+        public static final int kCurrentLimit = 80;
         public static final double kClimbUpSpeed = 0.9;   // Positive usually extends
         public static final double kClimbDownSpeed = -0.9; // Negative usually retracts
         public static final double kClimbSlewRate = 2.0;
