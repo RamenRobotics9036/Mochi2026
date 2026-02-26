47afa4bc981f3a3ab824d2aeb8296a31fb5519eb
Tarun5v <tarungangadharanlathaparvathi@gmail.com>
2026-02-26 14:17:04 -0800

    adding L_Zone_Shoot_Home

--- src/main/java/frc/robot/auto/AutoLogic.java
+++ src/main/java/frc/robot/auto/AutoLogic.java
@@ -103,6 +103,7 @@ public final class  AutoLogic {
         autoPicker.setDefaultOption("testing shoot", "testing shoot");
         autoPicker.addOption("testing intake fuel", "testing intake fuel");
         autoPicker.addOption("R_Zone_Shoot_Home", "R_Zone_Shoot_Home");
+        autoPicker.addOption("L_Zone_Shoot_Home", "L_Zone_Shoot_Home");
         autoPicker.addOption("C_ClimbShoot", "C_ClimbShoot");
     }
 
