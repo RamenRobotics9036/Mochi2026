fb62d611e1e4051fd25010c3859d4d2daccd41fa
Ido <idoben2@hotmail.com>
2026-03-01 11:41:54 -0500

    Add new methods to camodometry interface

--- src/main/java/frc/robot/visutils/CamOdometryInterface.java
+++ src/main/java/frc/robot/visutils/CamOdometryInterface.java
@@ -8,7 +8,36 @@ public interface CamOdometryInterface {
     Optional<Pose2d> getLatestVisPose();
     double getCurrentConfidenceScore();
     int getNumLockedTags();
-    double getTx();
     List<Integer> getTargetList();
+
+
+    /**
+     * Returns True if ANY camera currently has a lock on at-least one
+     * target.
+     */
+    // $TODO - isAnyCameraLockedOn
+
+    /**
+     * Returns True if ANY camera has a lock on at least TWO targets,
+     * i.e. a multi-lock.
+     */
+    // $TODO - isAnyCameraMultiLockedOn
+
+    /**
+     * Gets the ID of the target currently locked onto by
+     * the PRIMARY camera, or -1 if no target is locked.
+     * This method is generally used to tell what target the robot
+     * is currently aligned to, so that the robot can turn towards it.
+      */
+    // $TODO - Rename to getCurrentlyAlignedAprilTagId
     int getLastTarget();
+
+    /**
+     * Get the horizontal offset from the currently locked onto
+     * target for the PRIMARY camera.  Returns 0 if no target is locked.
+     * This method is generally used to tell how far off the robot is from
+     * being aligned to the target, so that the robot can turn towards it.
+     */
+    // $TODO - Rename to getCurrentlyAlignedTx
+    double getTx();
 }
