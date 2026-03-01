25247bf98ffd8f158a27bdd5744013d89b05c286
Ido <idoben2@hotmail.com>
2026-03-01 16:05:56 -0500

    Remove unused code for Taruns commit

--- src/main/java/frc/robot/commands/JiggleCommand.java
+++ src/main/java/frc/robot/commands/JiggleCommand.java
@@ -16,7 +16,6 @@ import frc.robot.subsystems.CommandSwerveDrivetrain;
 
 public class JiggleCommand extends Command {
     private final CommandSwerveDrivetrain m_swerve;
-    // private final ArmSubsystem m_arm;
     private boolean m_isForward;
     private final Timer m_timer = new Timer();
 
@@ -24,8 +23,7 @@ public class JiggleCommand extends Command {
     
     public JiggleCommand(CommandSwerveDrivetrain swerve/*, ArmSubsystem arm*/) {
         m_swerve = swerve;
-        // m_arm = arm;
-        addRequirements(m_swerve/*, m_arm*/);
+        addRequirements(m_swerve);
     }
 
     @Override
@@ -49,18 +47,11 @@ public class JiggleCommand extends Command {
             m_swerve.setControl(m_request.withVelocityX(DriveConstants.kJiggleSpeed.times(-1)));
             System.out.println("    Jiggle forward");
         }
-
-        // double wigglePower = (m_timer.get() < (DriveConstants.kSecondsToAlternate.in(Seconds) / 2.0)) 
-        //     ? ArmConstants.kArmSpeed * 0.5 
-        //     : -ArmConstants.kArmSpeed * 0.5;
-        
-        // m_arm.moveArmWithSpeed(wigglePower);
     }
 
     @Override
     public void end(boolean interrupted) {
         m_swerve.setControl(m_request.withVelocityX(0).withVelocityY(0));
-        // m_arm.stop();
     }
 
     @Override
