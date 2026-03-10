e2895efe2c593900e350b6e7f6abbfc979d723ae
Ramen9036Bot1 <ramen9036bot1@gmail.com>
2026-03-10 15:32:21 -0700

    Add spin-up time to the Shoot command in Auto

--- src/main/java/frc/robot/commands/ShootCommand.java
+++ src/main/java/frc/robot/commands/ShootCommand.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.Constants;
 import frc.robot.subsystems.IndexerSubsystem;
@@ -13,19 +14,30 @@ public class ShootCommand {
         ShooterSubsystem shooter,
         IndexerSubsystem indexer) {
 
-        return new RunCommand(() -> {
+        return Commands.sequence(
+            // Spin up shooter
+            new RunCommand(() -> {
+                shooter.setSpeed(Constants.ShooterConstants.kShootSpeed);
+            },
+            // Dependencies:
+            shooter, indexer)
+            .withTimeout(Constants.IndexerConstants.kIndexDelay),
+
+            // Start the indexer and start shooting
+            new RunCommand(() -> {
                 shooter.setSpeed(Constants.ShooterConstants.kShootSpeed);
                 indexer.setSpeed(Constants.IndexerConstants.kIndexSpeed);
             },
             // Dependencies:
             shooter, indexer)
             .withTimeout(8.0)
-
-            // finallyDo ensures motors are stopped even if command is
-            // cancelled.
-            .finallyDo(() -> {
-                shooter.stop();
-                indexer.stop();
-            });
+        )
+        
+        // finallyDo ensures motors are stopped even if command is
+        // cancelled.
+        .finallyDo(() -> {
+            shooter.stop();
+            indexer.stop();
+        });
     }
 }
