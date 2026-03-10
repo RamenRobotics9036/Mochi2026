77412178f1a3feba6d73b6b14d3bb6bad9cd1985
Ramen9036Bot1 <ramen9036bot1@gmail.com>
2026-03-09 18:19:53 -0700

    Cleanups excessive logic and makes minor fixes

--- src/main/java/frc/robot/commands/ShooterDefaultCommand.java
+++ src/main/java/frc/robot/commands/ShooterDefaultCommand.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.math.filter.Debouncer;
+import edu.wpi.first.math.filter.Debouncer.DebounceType;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.IndexerConstants;
@@ -12,8 +13,9 @@ public class ShooterDefaultCommand extends Command{
     private ShooterSubsystem m_shooter;
     private IndexerSubsystem m_indexer;
     private CommandXboxController m_controller;
-    private final Timer m_timer = new Timer();
-    private boolean wasShooterJustRunning = false;
+    private final Debouncer m_debouncer = new Debouncer(0.25, DebounceType.kRising);
+    private double indexerDirection = 0.0;
+    private boolean indexerEnabled = false;
 
     public ShooterDefaultCommand(ShooterSubsystem shooter, IndexerSubsystem indexer, CommandXboxController controller){
         m_shooter = shooter;
@@ -24,35 +26,29 @@ public class ShooterDefaultCommand extends Command{
     }
 
     @Override
-    public void initialize() {}
+    public void initialize() {
+        m_debouncer.calculate(false); //In case the default command gets interrupted
+    }
 
     @Override
     public void execute() {
-        if (m_controller.y().getAsBoolean()){
+        if (m_controller.y().getAsBoolean()) indexerDirection = 1.0;
+        else if (m_controller.rightBumper().getAsBoolean()) indexerDirection = -1.0;
+        else indexerDirection = 0.0;
+
+        indexerEnabled = m_debouncer.calculate(indexerDirection != 0.0);
+
+        if (indexerDirection != 0.0) {
             m_shooter.setSpeed(ShooterConstants.kShootSpeed);
-            if (wasShooterJustRunning) {
-                if (m_timer.get() >= 0.25) {
-                    m_indexer.setSpeed(IndexerConstants.kIndexSpeed);
-                }
-            } else {
-                wasShooterJustRunning = true;
-                m_timer.reset();
+            
+            if (indexerEnabled) {
+                m_indexer.setSpeed(indexerDirection * IndexerConstants.kIndexSpeed);
             }
-        } else if (m_controller.rightBumper().getAsBoolean()) {
-            m_shooter.setSpeed(ShooterConstants.kShootSpeed);
-            if (wasShooterJustRunning) {
-                if (m_timer.get() >= 0.25) {
-                    m_indexer.setSpeed(-IndexerConstants.kIndexSpeed);
-                }
-            } else {
-                wasShooterJustRunning = true;
-                m_timer.reset();
-            }
+
         } else {
-            wasShooterJustRunning = false;
             m_shooter.stop();
             m_indexer.stop();
-        };
+        }
     }
 
     @Override
