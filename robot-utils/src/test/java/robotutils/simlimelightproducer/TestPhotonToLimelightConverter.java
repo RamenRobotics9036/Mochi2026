ca8f212a338201a9505e82a0ee0d2cffd1c38669
Ido <idoben2@hotmail.com>
2026-03-28 16:03:06 -0700

    Fix unit test

--- robot-utils/src/test/java/robotutils/simlimelightproducer/TestPhotonToLimelightConverter.java
+++ robot-utils/src/test/java/robotutils/simlimelightproducer/TestPhotonToLimelightConverter.java
@@ -1,4 +1,4 @@
-package frc.robot.sim.visionproducers;
+package robotutils.simlimelightproducer;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
